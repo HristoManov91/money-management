9884c188bc4f3b58704bb54291061b8e0cac01c8
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2022-10-14 22:13:49 +0300

    swagger documentation

--- money-management-be/src/main/java/com/example/moneymanagementbe/controller/crud/EditController.java
+++ money-management-be/src/main/java/com/example/moneymanagementbe/controller/crud/EditController.java
@@ -3,6 +3,9 @@ package com.example.moneymanagementbe.controller.crud;
 import com.example.moneymanagementbe.dto.BaseDto;
 import com.example.moneymanagementbe.entity.BaseEntity;
 import com.example.moneymanagementbe.service.CrudService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +16,14 @@ public interface EditController<D extends BaseDto, E extends BaseEntity> {
 
     CrudService<D, E> crudService();
 
+    @Operation(
+        summary = "Update resource by Id value.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Resource edited successfully"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Resource with this id was not found", content = @Content)
+        })
     @PutMapping(path = "{id}", produces = {"application/json"})
     default ResponseEntity<D> edit(@PathVariable(name = "id") Long id, @Valid @RequestBody D dto) {
         // TODO : check id validate
--- money-management-be/src/main/java/com/example/moneymanagementbe/controller/crud/ReadController.java
+++ money-management-be/src/main/java/com/example/moneymanagementbe/controller/crud/ReadController.java
@@ -4,6 +4,11 @@ import com.example.moneymanagementbe.dto.BaseDto;
 import com.example.moneymanagementbe.entity.BaseEntity;
 import com.example.moneymanagementbe.service.CrudService;
 import com.querydsl.core.types.Predicate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,8 +22,13 @@ public interface ReadController<D extends BaseDto, E extends BaseEntity> {
 
     CrudService<D, E> crudService();
 
-    //TODO swagger
-    @GetMapping(produces = {"application/json"})
+    @Operation(summary = "Find all resources by ids")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Return all resources fount."),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+    })
+    @GetMapping( produces = {"application/json"})
     default ResponseEntity<Page<D>> findAll(
         @RequestParam(value = "criteria") Optional<String> criteria,
         @QuerydslPredicate(root = BaseEntity.class) Predicate predicate,
@@ -26,7 +36,14 @@ public interface ReadController<D extends BaseDto, E extends BaseEntity> {
         return ResponseEntity.ok(crudService().findAll(predicate, pageable));
     }
 
-    //TODO swagger
+    @Operation(summary = "Find resource by id.")
+    @Parameter(name = "id", description = "The ID from type Long of the resource", required = true)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found the resource"),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+        @ApiResponse(responseCode = "404", description = "Resource with this id was not found", content = @Content)// TODO content class exception
+    })
     @GetMapping(path = "{id}", produces = {"application/json"})
     default ResponseEntity<D> findById(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(crudService().findById(id));
