507f21a089fdfc09495550cab8af8efe93a6db6e
HristoManov91 <73854367+HristoManov91@users.noreply.github.com>
2023-02-11 22:05:28 +0200

    added modal for messages

---
+++ money-management-fe/src/components/modal/MessageModal.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- TODO class -->
+  <div :class="messageClass">
+    <p>{{ this.message }}</p>
+  </div>
+</template>
+
+<script src="../../scripts/modal/message.js"></script>
+
+<style scoped>
+@import url("../../assets/css/modal/message.css");
+</style>
---
+++ money-management-fe/src/scripts/modal/message.js
@@ -0,0 +1,13 @@
+export default {
+    name: "Message",
+    props: {
+        message: {
+            type: String,
+            required: true
+        },
+        messageClass: {
+            type: String,
+            required: true
+        }
+    },
+}
